42906dffc538b8b75a3d90ea879384fb1b6a6caf
Guillaume <guillaume.sallaber@gmail.com>
2017-11-29 17:36:04 +0100

    convert the app logo into a link to the homepage

--- src/components/AppBanner/AppBanner.js
+++ src/components/AppBanner/AppBanner.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import './AppBanner.css';
 
 
@@ -6,7 +7,7 @@ class AppBanner extends Component {
   render() {
     return (
       <div className="app-banner">
-        <div className="app-banner-logo"><span className="swg swg-starwars swg-3x"></span></div>
+        <div className="app-banner-logo"><Link to="/"><span className="swg swg-starwars swg-3x"></span></Link></div>
         <div className="app-banner-subtitle"><span>Browsable API</span></div>
       </div>
     );
